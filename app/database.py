38d9d8f907b4746d2bf3c87c374d49ad5bc57071
petrinka <nikitaboss102@gmail.com>
2025-02-17 16:57:46 +0300

    added env file support

--- app/database.py
+++ app/database.py
@@ -1,8 +1,21 @@
 from typing import Annotated
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from dotenv import load_dotenv
+import os
 
-engine = create_async_engine('mysql+aiomysql://user:qwerty@localhost:3306/bankDB')
+load_dotenv(dotenv_path=".env")
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+
+
+engine = create_async_engine(
+    f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
 
 
 new_session = async_sessionmaker(engine, expire_on_commit=False)
